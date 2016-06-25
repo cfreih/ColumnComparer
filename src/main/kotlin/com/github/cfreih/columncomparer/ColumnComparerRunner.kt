f25a1f2846b495a5e45b3119c1d6dfedd1e84e0d
cfreih <clint.freiheit@gmail.com>
2016-06-25 16:55:33 -0500

    Added functionality to main method.

--- src/main/kotlin/com/github/cfreih/columncomparer/ColumnComparerRunner.kt
+++ src/main/kotlin/com/github/cfreih/columncomparer/ColumnComparerRunner.kt
@@ -1,11 +1,15 @@
 package com.github.cfreih.columncomparer
 
+import com.github.cfreih.columncomparer.dataclasses.ColumnInfo
+import com.github.cfreih.columncomparer.services.ColumnComparer
+import com.github.cfreih.columncomparer.services.MissingValuesDisplay
 import com.github.cfreih.columncomparer.verification.ArgsVerifier
 
 
 fun main(args: Array<String>) {
 	ArgsVerifier(args).verify()
-	//setup columns to compare
-	//compare columns
-	//print results
+	val columnComparer = ColumnComparer.create(args)
+	val missingValues : Map<ColumnInfo, List<String>> = columnComparer.findMissingValues()
+	val display =MissingValuesDisplay(missingValues, columnComparer.masterColumn.fileName)
+	println(display.buildMissingValuesDisplay())
 }

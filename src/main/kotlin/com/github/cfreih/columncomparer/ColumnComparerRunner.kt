15e6f83a66885c7b8da1ce9a808919aecac7ac76
cfreih <cfreih@users.noreply.github.com>
2016-06-30 18:20:39 -0500

    Update ColumnComparerRunner.kt

--- src/main/kotlin/com/github/cfreih/columncomparer/ColumnComparerRunner.kt
+++ src/main/kotlin/com/github/cfreih/columncomparer/ColumnComparerRunner.kt
@@ -10,6 +10,6 @@ fun main(args: Array<String>) {
 	ArgsVerifier(args).verify()
 	val columnComparer = ColumnComparer.create(args)
 	val missingValues : Map<ColumnInfo, List<String>> = columnComparer.findMissingValues()
-	val display =MissingValuesDisplay(missingValues, columnComparer.masterColumn.fileName)
+	val display = MissingValuesDisplay(missingValues, columnComparer.masterColumn.fileName)
 	println(display.buildMissingValuesDisplay())
 }
